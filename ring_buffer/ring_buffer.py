25dd62345eb798696b4f41744a368a2c2f245e75
Jason Clegg <tjason.clegg@gmail.com>
2020-09-11 11:47:40 -0600

    Submitting for MVP

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,18 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.data = []
+        self.index = 0
 
     def append(self, item):
-        pass
+        # if the number of items is the same as the capacity, then set the current index to item
+        if len(self.data) == self.capacity:
+            self.data[self.index] = item
+
+        # if the number of items is not the same as capacity, then set the index to add 1 and return the raminder of the index divided by the capacity
+        else:
+            self.data.append(item)
+        self.index = (self.index + 1) % self.capacity
 
     def get(self):
-        pass
+        return self.data
